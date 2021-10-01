89c9bc212fce9dd786337da039f7ed9fa0abf6e2
yuh2017 <34501842+yuh2017@users.noreply.github.com>
2021-10-01 12:31:27 -0400

    Delete src directory

--- src/RunExample.java
+++
@@ -1,9 +0,0 @@
-
-public class RunExample {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//new String[]{ "examples/tutorial/config/example5-config.xml" }
-		RunFromConfigfileExample.main(args) ;
-	}
-}
